1d7d0543eaff921f485d7cf194e6c5768cd822fe
robin lemmens <robin.lemmens@student.ap.be>
2022-06-17 22:36:53 +0200

    Update membersmaken.ps1
    vergeten enkel de displayname te nemen in $namen variable

--- Documents/school/powershellopdracht/membersmaken.ps1
+++ Documents/school/powershellopdracht/membersmaken.ps1
@@ -1,5 +1,6 @@
 ﻿'members vinden'
 $members = $groepenid | ForEach-Object {Get-AzureADGroupMember -ObjectId $_}
-
+'enkel de namen'
+$namen = $members.Displayname
 'members aanmaken in de correcte OU'
- $members|ForEach-Object {New-ADUser -Name $_ -DisplayName $_  -path "OU=users,OU=s131924,OU=OSscripting2022,DC=poliformarl,DC=local"}
+ $namen|ForEach-Object {New-ADUser -Name $_ -DisplayName $_  -path "OU=users,OU=s131924,OU=OSscripting2022,DC=poliformarl,DC=local"}
