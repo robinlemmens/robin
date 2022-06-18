6985186360abe4b96899f0afe4f793b33db61df0
robin lemmens <robin.lemmens@student.ap.be>
2022-06-18 19:24:06 +0200

    Delete membersmaken.ps1

--- Documents/school/powershellopdracht/membersmaken.ps1
+++
@@ -1,6 +0,0 @@
-﻿'members vinden'
-$members = $groepenid | ForEach-Object {Get-AzureADGroupMember -ObjectId $_}
-'enkel de namen'
-$namen = $members.Displayname
-'members aanmaken in de correcte OU'
- $namen|ForEach-Object {New-ADUser -Name $_ -DisplayName $_  -path "OU=users,OU=s131924,OU=OSscripting2022,DC=poliformarl,DC=local"}
