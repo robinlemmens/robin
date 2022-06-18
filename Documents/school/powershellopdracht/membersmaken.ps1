a95b9647a2e6ddc63ce9acc2d7ba79f3fdafad24
robin lemmens <robin.lemmens@student.ap.be>
2022-06-18 19:28:46 +0200

    nieuw memberscript toevoegen

---
+++ Documents/school/powershellopdracht/membersmaken.ps1
@@ -0,0 +1,9 @@
+﻿'members vinden'
+$members = $groepenid | ForEach-Object {Get-AzureADGroupMember -ObjectId $_}
+'enkel de naam nemen'
+$student = $members.Displayname
+$namen=$student |ForEach-Object{$_ -replace ".........$"}
+
+
+'members aanmaken in de correcte OU'
+ $student|ForEach-Object {New-ADUser -Name $_ -DisplayName $_  -path "OU=users,OU=s131924,OU=OSscripting2022,DC=poliformarl,DC=local"}
